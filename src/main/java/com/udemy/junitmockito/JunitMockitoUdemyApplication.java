8a571562caa61386055c3b8adceeb866066312d8
Ives Chiba <ivesjundi@gmail.com>
2024-07-17 21:10:03 -0300

    init :tada: junit mockito project initialized

---
+++ src/main/java/com/udemy/junitmockito/JunitMockitoUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.junitmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMockitoUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitMockitoUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udemy/junitmockito/JunitMockitoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.junitmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitMockitoUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
