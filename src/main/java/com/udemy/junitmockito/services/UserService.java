ef6c43012a0bb00b044bbb7fb1021877b59fe9a3
Ives Chiba <ivesjundi@gmail.com>
2024-07-17 23:44:19 -0300

    refactor :recycle: user service -> user service impl

--- src/main/java/com/udemy/junitmockito/services/UserService.java
+++ src/main/java/com/udemy/junitmockito/services/UserService.java
@@ -1,37 +1,12 @@
 package com.udemy.junitmockito.services;
 
 import com.udemy.junitmockito.models.User;
-import com.udemy.junitmockito.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    @Autowired
-    UserRepository userRepository;
-
-    public List<User> findAll(){
-        return userRepository.findAll();
-    }
-
-    public User findById(Integer id){
-        Optional<User> optionalUser = userRepository.findById(id);
-
-        return optionalUser.orElse(null);
-    }
-
-    public User createUser(User user){
-        User newUser = new User(
-                user.getId(),
-                user.getName(),
-                user.getEmail(),
-                user.getPassword());
-        userRepository.save(newUser);
-        return newUser;
-    }
+    List<User> findAll();
+    User findById(Integer id);
+    User createUser(User user);
 }
---
+++ src/main/java/com/udemy/junitmockito/services/impl/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.udemy.junitmockito.services.impl;
+
+import com.udemy.junitmockito.models.User;
+import com.udemy.junitmockito.repositories.UserRepository;
+import com.udemy.junitmockito.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Integer id){
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        return optionalUser.orElse(null);
+    }
+
+    public User createUser(User user){
+        User newUser = new User(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getPassword());
+        userRepository.save(newUser);
+        return newUser;
+    }
+}
