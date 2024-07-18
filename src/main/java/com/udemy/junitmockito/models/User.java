1a352c9e345f723f4758a52c5103c447824beee5
Ives Chiba <ivesjundi@gmail.com>
2024-07-17 21:19:18 -0300

    feat :sparkles: user model

---
+++ src/main/java/com/udemy/junitmockito/models/User.java
@@ -0,0 +1,24 @@
+package com.udemy.junitmockito.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String password;
+}
